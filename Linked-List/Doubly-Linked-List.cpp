37d95f579ccd880d789b8429cf3292369a432c22
vaishnaowankar <vaishnao.wankar2000@gmail.com>
2022-08-28 00:08:59 +0530

    Modified SLL Code and added DLL

--- Linked-List/Doubly-Linked-List.cpp
+++ Linked-List/Doubly-Linked-List.cpp
@@ -1,62 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node{
     public:
     int data;
+    Node* prev;
     Node* next;
 
     //constructor
     Node(int data){
         this -> data = data;
+        this -> prev = NULL;
         this -> next = NULL;
     }
 
-    //destructor for freeing memory of removed node
     ~Node(){
-        //To know which value is going to be freed
         int value = this -> data;
-        //freeing memory
-        if(this -> next != NULL){
+        if(next != NULL){
             delete next;
-            this -> next = NULL;
+            next = NULL;
         }
+        cout<<"Memory free for node with data: "<<value<<endl;
     }
 };
 
+//function to print the linked list
 void print(Node* &head){
     Node* temp = head;
 
     while(temp != NULL){
-        cout<<temp -> data<<" ";  //print data of temp
-        temp = temp -> next;      //move temp pointer to next location
+        cout<<temp -> data<<" ";
+        temp = temp -> next;
     }
     cout<<endl;
 }
 
-//function to insert node at the beginning of the linked list
-void insertAtHead(Node* &head,Node* &tail, int d){
+//function to find length of linked list or traversing a linked list
+int getLength(Node* &head){
+    int len=0;
+    Node* temp = head;
+
+    while(temp != NULL){
+        len++;
+        temp = temp->next;
+    }
+
+    return len;
+}
+
+//Inserting new node at head
+void insertAtHead(Node* &head ,Node* &tail, int d){
     
-    //Handling empty list
-    if(tail == NULL){
+    //handling empty list
+    if(head == NULL){
         Node* temp = new Node(d);
-        tail = temp;
         head = temp;
-        
+        tail = temp;
     }
     else{
         Node* temp = new Node(d);
-        temp->next = head;
+        temp -> next = head;
+        head -> prev = temp;
         head = temp;
     }
-    
     
 }
 
-//function to insert node at the end of the linked list
-void insertAtTail(Node* &head,Node* &tail,int d){
-
-    //handling empty lists
+//Inserting new node at tail
+void insertAtTail(Node* &head,Node* &tail , int d){
+    
+    //handling empty list
     if(tail == NULL){
         Node* temp = new Node(d);
         tail = temp;
@@ -65,49 +78,53 @@ void insertAtTail(Node* &head,Node* &tail,int d){
     }
     else{
         Node* temp = new Node(d);
+        temp -> prev = tail;
         tail -> next = temp;
         tail = temp;
     }
     
 }
 
-//function to insert node at any given position of the linked list
-void insertAtPosition(Node* &head,Node* &tail,int d,int position){
+//Inserting at a given position
+void insertAtPosition(Node* &head,Node* &tail,int position,int d){
     
-    //Case to handle inserting at first position
+    //insert at start
     if(position == 1){
         insertAtHead(head,tail,d);
         return;
     }
 
-    //traversing a temp pointer to position behind the position where we want to insert.
     Node* temp = head;
     int cnt = 1;
-    while(cnt < position-1){
+    while(cnt < position -1){
         temp = temp -> next;
         cnt++;
     }
 
-    //Case to update the tail to last inserted node.
+    //insert at last position
     if(temp -> next == NULL){
         insertAtTail(head,tail,d);
         return;
     }
 
-    //creating and adding a new node at position
+    //creating a node for d
     Node* nodeToInsert = new Node(d);
-    nodeToInsert -> next = temp -> next;
-    temp -> next = nodeToInsert;
 
+    nodeToInsert -> next = temp -> next;
+    temp -> next -> prev = nodeToInsert;
+    nodeToInsert -> prev = temp;
+    temp -> next = nodeToInsert;
 }
+
 
 //function to delete a node
 void deleteNode(Node* &head,Node* &tail,int position){
     //deleting first or start node
     if(position == 1){
         Node* temp = head;
-        head = head -> next;
-        temp->next = NULL;
+        temp -> next -> prev = NULL;
+        head = temp -> next;
+        temp -> next = NULL;
         delete temp;
     }
     else{
@@ -126,12 +143,16 @@ void deleteNode(Node* &head,Node* &tail,int position){
         
 
         //logic to delete the node
-        prev -> next = current -> next;
+        
+        
+        current -> prev = NULL;
+        prev -> next  = current -> next;
         //case to update tail of the linked list
         if(prev -> next == NULL){
             tail = prev ;
             return;
         }
+
         current -> next = NULL;
         delete current;
     }
@@ -140,45 +161,41 @@ void deleteNode(Node* &head,Node* &tail,int position){
 
 int main(){
     // Node* node1 = new Node(10);
-    // cout<<node1 -> data<<endl;
-    // cout<<node1 -> next<<endl;
     // Node* head = node1;
     // Node* tail = node1;
     // print(head);
 
-    //If no node is present at first;
+    //Remember if we dont have this initial node , then head and tail both point to NULL and we need to handle the empty list condition in insertAtHead and insertAtTail functions.
     Node* head = NULL;
     Node* tail = NULL;
 
-    insertAtHead(head,tail,12);
+    
+
+    // cout<<getLength(head)<<endl;
+
+    insertAtHead(head,tail,11);
+    print(head);
+    insertAtHead(head,tail,22);
+    print(head);
+    insertAtHead(head,tail,8);
     print(head);
 
-    insertAtTail(head,tail,15);
+    insertAtTail(head,tail,9);
     print(head);
 
-    insertAtPosition(head,tail,22,3);
+    insertAtPosition(head,tail,2,100);
     print(head);
 
-    insertAtPosition(head,tail,25,1);
+    insertAtPosition(head,tail,1,50);
     print(head);
 
-    cout<<"Head -> "<<head -> data<<endl;
-    cout<<"Tail -> "<<tail -> data<<endl;
-
-    deleteNode(head,tail,1);
+    insertAtPosition(head,tail,7,20);
     print(head);
 
-    deleteNode(head,tail,4);
+    deleteNode(head,tail,7);
     print(head);
 
-    cout<<"Head -> "<<head -> data<<endl;
-    cout<<"Tail -> "<<tail -> data<<endl;
-
-    deleteNode(head,tail,3);
-    print(head);
-
-    cout<<"Head -> "<<head -> data<<endl;
-    cout<<"Tail -> "<<tail -> data<<endl;
-
+    cout<<"Head: "<<head -> data<<endl;
+    cout<<"Tail: "<<tail -> data<<endl;
     return 0;
 }

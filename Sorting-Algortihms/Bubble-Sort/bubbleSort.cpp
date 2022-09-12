d9e4c107c8e395e8298e7fff58890191feae2ca3
vaishnaowankar <vaishnao.wankar2000@gmail.com>
2022-09-12 23:33:24 +0530

    optimised bubble sort code

--- Sorting-Algortihms/Bubble-Sort/bubbleSort.cpp
+++ Sorting-Algortihms/Bubble-Sort/bubbleSort.cpp
@@ -3,7 +3,17 @@
 
 //In this algo we compare consequetive elements and swap accorting to sorted order required in round that happens n-1 times.
 
-// In each round the ith largest element gets placed at its right position , Ex in 1st round the 1st largest element gets placed at the end.
+// In each round the ith largest element gets placed at its right position , Ex in 1st round the 1st largest element gets placed at the end.  --> Use Case **
+
+
+/*
+Time complexity :
+Best Case : O(n) -> when used swapped variable
+Worst Case : O(n^2)
+
+Space Complexity : O(1)
+
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -18,12 +28,18 @@ void bubbleSort(vector<int>& arr, int n)
        
         
         //processing or comparing consequetive elements
-        
+        bool swapped = false;  //using these we are optimising the algo
+
         for(int j = 0 ; j<n-i ; j++){  //note we are traversing till n-i index in each iteration because in each round the largest elements gets placed at right positions in the end so , we need not compare then.
             
             if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped = true;
             }
+        }
+        if(swapped == false){
+            //so if in any round in between the array gets sorted and no swap happens then we break the loop.
+            break;
         }
     }
     
